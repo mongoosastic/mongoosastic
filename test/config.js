91c7cc3f759122a5923f35ddbcc1b7c2033c0a97
James R. Carr <james.r.carr@gmail.com>
2012-06-05 00:29:06 -0500

    initial feature works

---
+++ test/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoUrl: 'mongodb://localhost/es-test'
+};
---
+++ test/index-test.js
@@ -0,0 +1,80 @@
+var mongoose  = require('mongoose')
+  , elastical = require('elastical')
+  , should    = require('should')
+  , config    = require('./config')
+  , Schema    = mongoose.Schema
+  , ObjectId  = Schema.ObjectId
+  , mongoosastic = require('../lib/mongoosastic')
+  , esClient  = new(require('elastical').Client)
+
+var TweetSchema = new Schema({
+    user: String
+  , post_date: Date
+  , message: String
+});
+
+
+TweetSchema.plugin(mongoosastic, {
+    index:'tweets'
+  , type: 'tweet'
+})
+var Tweet = mongoose.model('Tweet', TweetSchema);
+
+
+
+describe('indexing', function(){
+  before(function(done){
+    mongoose.connect(config.mongoUrl, function(){
+      Tweet.remove(function(){
+        deleteIndexIfExists('tweets', done)
+      });
+    });
+  });
+
+  after(function(done){
+    Tweet.remove(function(){
+      mongoose.disconnect();
+      done();
+    });
+  });
+
+  describe('Default plugin', function(){
+    before(function(done){
+      var tweet = new Tweet({
+          user: 'jamescarr'
+        , message: "I like Riak better"
+        , post_date: new Date()
+      });
+      tweet.save(function(){
+        tweet.on('es-indexed', function(err, res){
+          setTimeout(done, 1100)
+        });
+      });
+    });
+    it('should be able to execute a simple query', function(done){
+      Tweet.search({query:'Riak'}, function(err, results) {
+        results.total.should.eql(1)
+        results.hits[0].message.should.eql('I like Riak better')
+        done()
+      });
+    });
+    it('should be able to execute a simple query', function(done){
+      Tweet.search({query:'jamescarr'}, function(err, results) {
+        results.total.should.eql(1)
+        results.hits[0].message.should.eql('I like Riak better')
+        done()
+      });
+    });
+  });
+});
+
+
+function deleteIndexIfExists(index, cb){
+  esClient.indexExists(index, function(err, exists){
+    if(exists){
+      esClient.deleteIndex(index, cb);
+    }else{
+      cb()
+    }
+  });
+}
