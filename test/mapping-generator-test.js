c04cdb0813cc161bd8b5ff0197a3a18b31f5e170
Alban Mouton <alban.mouton@gmail.com>
2014-04-01 20:30:49 +0200

    Improve mapping of nested arrays.

--- test/mapping-generator-test.js
+++ test/mapping-generator-test.js
@@ -75,6 +75,7 @@ describe('MappingGenerator', function(){
         done();
       });
     });
+    
     it('recognizes an multi_field and maps it as one', function(done){
       generator.generateMapping(new Schema({
         test: {
@@ -134,6 +135,38 @@ describe('MappingGenerator', function(){
         mapping.properties.name.type.should.eql('object');
         mapping.properties.name.properties.first_name.type.should.eql('string');
         mapping.properties.name.properties.last_name.type.should.eql('string');
+        done();
+      });
+    });
+    it('recognizes a nested array with a simple type and maps it as a simple attribute', function(done){
+      generator.generateMapping(new Schema({
+        contacts: [String]
+      }), function(err, mapping){
+        mapping.properties.contacts.type.should.eql('string');
+        done();
+      });
+    });
+    it('recognizes a nested array with a simple type and additional attributes and maps it as a simple attribute', function(done){
+      generator.generateMapping(new Schema({
+        contacts: [{ type: String, es_index: 'not_analyzed' }]
+      }), function(err, mapping){
+        mapping.properties.contacts.type.should.eql('string');
+        mapping.properties.contacts.index.should.eql('not_analyzed');
+        done();
+      });
+    });
+    it('recognizes a nested array with a complex object and maps it', function(done){
+      generator.generateMapping(new Schema({
+        name: String,
+        contacts: [{
+            email: {type: String, es_index: 'not_analyzed' },
+            telephone: String
+        }]
+      }), function(err, mapping){
+        mapping.properties.name.type.should.eql('string');
+        mapping.properties.contacts.properties.email.type.should.eql('string');
+        mapping.properties.contacts.properties.email.index.should.eql('not_analyzed');
+        mapping.properties.contacts.properties.telephone.type.should.eql('string');
         done();
       });
     });
