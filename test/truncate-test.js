50d82e14ea19c959d6c44776cc7c072d2c3bfdb1
antoineverger <antoine.verger@speicher210.com>
2014-09-26 09:28:36 +0200

    Add the esTruncate static method to remove all documents from an index

---
+++ test/truncate-test.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose'),
+  elastical = require('elastical'),
+  esClient = new(require('elastical').Client),
+  should = require('should'),
+  config = require('./config'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId,
+  async = require('async'),
+  mongoosastic = require('../lib/mongoosastic');
+
+var DummySchema = new Schema({
+  text: String
+});
+DummySchema.plugin(mongoosastic);
+
+var Dummy = mongoose.model('Dummy', DummySchema);
+
+describe('Truncate', function() {
+  before(function(done) {
+    mongoose.connect(config.mongoUrl, function() {
+      Dummy.remove(function() {
+        config.deleteIndexIfExists(['dummys'], function() {
+          var dummies = [
+            new Dummy({
+              text: 'Text1'
+            }),
+            new Dummy({
+              text: 'Text2'
+            }),
+          ];
+          async.forEach(dummies, function(item, cb) {
+            item.save(cb);
+          }, function() {
+            setTimeout(done, 1100);
+          });
+        });
+      });
+    });
+  });
+  after(function(done) {
+    Dummy.remove(done);
+  });
+  describe('truncate', function() {
+    it('should be able to truncate all documents', function(done) {
+      Dummy.esTruncate(function(err) {
+        Dummy.search({
+          query: 'Text1'
+        }, function(err, results) {
+          results.hits.total.should.eql(0);
+          done(err);
+        });
+      });
+    });
+  });
+});
