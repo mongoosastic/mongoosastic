927366e76bd6593a7bd1f878e05a34da6817c030
isayme <isaymeorg@gmail.com>
2016-12-16 17:59:00 +0800

    fix typo

--- test/ref-test.js
+++ test/ref-test.js
@@ -19,9 +19,9 @@ var PostCommentSchema = new Schema({
 });
 
 var PostSchema = new Schema({
-  body: {type: String, es_index: true},
-  author: {type: Schema.Types.ObjectId, ref: 'User', es_schema: UserSchema, es_index: true},
-  comments: [{type: Schema.Types.ObjectId, ref: 'PostComment', es_schema: PostComment, es_index: true}]
+  body: {type: String, es_indexed: true},
+  author: {type: Schema.Types.ObjectId, ref: 'User', es_schema: UserSchema, es_indexed: true},
+  comments: [{type: Schema.Types.ObjectId, ref: 'PostComment', es_schema: PostComment, es_indexed: true}]
 });
 
 PostSchema.plugin(mongoosastic, {
