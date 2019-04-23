9fa9103be095cd6f0245d1bc3f24d0c0b142318f
Axel Hellström <axel@hellstrom.io>
2019-04-23 15:04:02 +0200

    _doc is default

--- test/ref-test.js
+++ test/ref-test.js
@@ -73,7 +73,7 @@ describe('references', function () {
       Post.findOne({}, function (err, post) {
         esClient.get({
           index: 'posts',
-          type: 'post',
+          type: '_doc',
           id: post._id.toString()
         }, function (_err, res) {
           res._source.author.name.should.eql('jake')
@@ -120,7 +120,7 @@ describe('references', function () {
         Post.findOne({}, function (err, post) {
           esClient.get({
             index: 'posts',
-            type: 'post',
+            type: '_doc',
             id: post._id.toString()
           }, function (_err, res) {
             res._source.comments[0].text.should.eql('good post')
@@ -134,7 +134,7 @@ describe('references', function () {
         Post.findOne({}, function (err, post) {
           esClient.get({
             index: 'posts',
-            type: 'post',
+            type: '_doc',
             id: post._id.toString()
           }, function (_err, res) {
             res._source.comments[0].text.should.eql('good post')
