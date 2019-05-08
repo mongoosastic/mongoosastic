56edab21813f1fa16c22a71a3209e1c559b1087a
Axel Hellström <axel@hellstrom.io>
2019-05-08 13:24:41 +0200

    minor (generateMapping sync)

--- test/serialize-test.js
+++ test/serialize-test.js
@@ -31,15 +31,7 @@ const PersonSchema22 = new Schema({
 
 const Person = mongoose.model('Person22', PersonSchema22)
 
-let mapping
-
-// Serialize method requires a schema mapping
-generator.generateMapping(PersonSchema22, function (err, tmp) {
-  if (err) {
-    // do nothing
-  }
-  mapping = tmp
-})
+const mapping = generator.generateMapping(PersonSchema22)
 
 describe('serialize', function () {
   const dude = new Person({
