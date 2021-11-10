d3a48b304dd7452afe2a565d7bc62c61219e9533
Yahia KERIM <yahiakerim@gmail.com>
2021-11-10 14:58:43 +0100

    test(force-index-refresh): refactor using promises

--- test/force-index-refresh.test.ts
+++ test/force-index-refresh.test.ts
@@ -47,73 +47,75 @@ describe('forceIndexRefresh connection option', function () {
 		mongoose.disconnect()
 	})
 
-	it('should always suceed: refresh the index immediately on insert', function (done) {
+	it('should always suceed: refresh the index immediately on insert', async function (done) {
 		const d = new DummyRefresh({ text: 'Text1' })
 		const refresh = true
 
-		doInsertOperation(DummyRefresh, d, indexName, refresh, done)
+		await doInsertOperation(DummyRefresh, d, indexName, refresh, done)
 	})
 
-	it('should fail randomly: refresh the index every 1s on insert', function (done) {
+	it('should fail randomly: refresh the index every 1s on insert', async function (done) {
 		const d = new Dummy({ text: 'Text1' })
 		const refresh = false
 
-		doInsertOperation(Dummy, d, indexName, refresh, done)
+		await doInsertOperation(Dummy, d, indexName, refresh, done)
 	})
 
-	it('should always suceed: refresh the index immediately on update', function (done) {
+	it('should always suceed: refresh the index immediately on update', async function (done) {
 		const d = new DummyRefresh({ text: 'Text1' })
 		const refresh = true
 
-		doUpdateOperation(DummyRefresh, d, 'this is the new text', indexName, refresh, done)
+		await doUpdateOperation(DummyRefresh, d, 'this is the new text', indexName, refresh, done)
 	})
 
-	it('should fail randomly: refresh the index every 1s on update', function (done) {
+	it('should fail randomly: refresh the index every 1s on update', async function (done) {
 		const d = new Dummy({ text: 'Text1' })
 		const refresh = false
 
-		doUpdateOperation(Dummy, d, 'this is the new text', indexName, refresh, done)
+		await doUpdateOperation(Dummy, d, 'this is the new text', indexName, refresh, done)
 	})
 })
 
-async function doInsertOperation (Model: Model<IDummy>, object: PluginDocument, indexName: string, refresh: boolean, callback: CallableFunction) {
+async function doInsertOperation (Model: Model<IDummy>, object: PluginDocument, indexName: string, refresh: boolean, done: CallableFunction) {
 	// save object
 	const savedObject = await object.save()
 
-	savedObject.on('es-indexed', function () {
+	savedObject.on('es-indexed', async function () {
 		// look for the object just saved
-		Model.search({
+		const results = await Model.search({
 			term: { _id: savedObject._id }
-		}, function (err, results) {
-			if (refresh) {
-				expect(results?.body.hits.total).toEqual(1)
-			} else {
-				expect(results?.body.hits.total).toEqual(0)
-			}
-			callback()
 		})
+
+		if (refresh) {
+			expect(results?.body.hits.total).toEqual(1)
+		} else {
+			expect(results?.body.hits.total).toEqual(0)
+		}
+
+		done()
 	})
 }
 
-async function doUpdateOperation (Model: Model<IDummy>, object: PluginDocument, newText: string, indexName: string, refresh: boolean, callback: CallableFunction) {
+async function doUpdateOperation (Model: Model<IDummy>, object: PluginDocument, newText: string, indexName: string, refresh: boolean, done: CallableFunction) {
 	// save object
 	const savedObject = await object.save()
 
 	const updatedObject = await Model.findOneAndUpdate({ _id: savedObject._id }, { text: newText }, { new: true })
 
-	updatedObject?.on('es-indexed', function () {
+	updatedObject?.on('es-indexed', async function () {
 		// look for the object just saved
-		Model.search({
+		const results = await Model.search({
 			term: { _id: savedObject._id }
-		}, function (err, results) {
-			if (refresh) {
-				const hit = results?.body.hits.hits[0]._source
-				expect(results?.body.hits.total).toEqual(1)
-				expect(hit?.text).toEqual(newText)
-			} else {
-				expect(results?.body.hits.total).toEqual(0)
-			}
-			callback()
 		})
+
+		if (refresh) {
+			const hit = results?.body.hits.hits[0]._source
+			expect(results?.body.hits.total).toEqual(1)
+			expect(hit?.text).toEqual(newText)
+		} else {
+			expect(results?.body.hits.total).toEqual(0)
+		}
+
+		done()
 	})
 }
